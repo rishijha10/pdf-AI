0c06defa2a60c57cf560d3340706bebb1d19b121
Adnan Athania <adnanathania9@gmail.com>
2023-11-17 16:07:16 +0530

    three boxes changes done and security section added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,14 @@ import HeroSection from "../components/home-components/HeroSection";
 import Trial from "../components/general-components/Trial";
 import ReactPDF from "@react-pdf/renderer";
 import ThreeBox from "../components/home-components/ThreeBox";
+import SecuritySection from "../components/home-components/SecuritySection";
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
       <ThreeBox />
+      <SecuritySection />
     </div>
   );
 };
