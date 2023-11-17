a1c0041a0f4c90116adeaddd50fc1a0aa4b5ed8a
Adnan Athania <adnanathania9@gmail.com>
2023-11-17 16:15:28 +0530

    changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,22 +3,15 @@ import HeroSection from "../components/home-components/HeroSection";
 import Trial from "../components/general-components/Trial";
 import ReactPDF from "@react-pdf/renderer";
 import ThreeBox from "../components/home-components/ThreeBox";
-<<<<<<< HEAD
 import SecuritySection from "../components/home-components/SecuritySection";
-=======
 import Footer from "../components/general-components/Footer";
->>>>>>> ebae10a54a273dbf232b701ae38dac4068231f2e
-
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
       <ThreeBox />
-<<<<<<< HEAD
       <SecuritySection />
-=======
       <Footer />
->>>>>>> ebae10a54a273dbf232b701ae38dac4068231f2e
     </div>
   );
 };
