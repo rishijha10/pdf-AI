13ab427d3986e249a33670638128b3b416c35cfe
Muzammil Malik <abdmalik1593@gmail.com>
2023-11-18 14:30:06 +0530

    clearing code

--- src/App.js
+++ src/App.js
@@ -12,59 +12,6 @@ import {
 } from "@tanstack/react-query";
 
 function App() {
-  const message = "How is the weather today in new york";
-  useEffect(() => {
-    const messageBody = {
-      role: "You",
-      message: message,
-    };
-    chatService(messageBody);
-  }, []);
-  // const { mutate: getChat, isLoading: chatLoading } = useMutation(chatService, {
-  //   onSuccess: (data) => {
-  //     console.log(data, "respoooooooo");
-  //     const date = new Date();
-  //     let time = date.toLocaleTimeString();
-  //     const messageBody = {
-  //       role: "Bot",
-  //       message: data.answer,
-  //       time: time,
-  //       // startTime : data.start_time,
-  //       links: data.links,
-  //     };
-  //     console.log(messageBody);
-  //     // setMessages((prev) => {
-  //     //   return [...prev, messageBody];
-  //     // });
-  //   },
-  //   onError: (error) => {
-  //     console.log(error, "error");
-  //     // errorChatNotification();
-  //   },
-  // });
-  useEffect(() => {
-    // getChat(message);
-    // console.log(chatService(message));
-  }, []);
-  // useEffect(() => {
-  //   async function apiCall() {
-  //     const req = await fetch(
-  //       "https://genai-video-analyzer-rrcr7xvxjq-uc.a.run.app",
-  //       {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-type": "application/json",
-  //         },
-  //         body: JSON.stringify(message),
-  //       }
-  //     );
-  //     return req;
-  //   }
-  //   apiCall();
-  // }, []);
-  // useEffect(() => {
-  //   chatService(message);
-  // }, []);
   const queryClient = new QueryClient();
   const router = createBrowserRouter([
     {
